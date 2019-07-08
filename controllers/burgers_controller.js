f23e336f03871f41d4c39da66ffe96ec5f253ae9
Chloe Zhou <chloezhouny@gmail.com>
2019-07-08 08:02:52 -0700

    Create and export router for the app

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,56 @@
+var express = require("express");
+
+var router = express.Router();
+
+var order = require("../models/tasty.js");
+
+
+router.get("/", function(req, res) {
+  order.selectAll(function(data) {
+    var hbsObject = {
+      orders: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+
+
+
+router.post("/api/orders", function(req, res) {
+  order.insertOne([
+    "order_name", "devoured"
+  ], [
+    req.body.order_name, req.body.devoured
+  ], function(result) {
+   
+    res.json({ id: result.insertId });
+  });
+});
+
+
+
+
+
+router.put("/api/orders/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  order.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
